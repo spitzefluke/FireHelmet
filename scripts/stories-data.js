dac81525f534222a7a997782c519687304695dfd
spitzefluke <spitzefluke@gmail.com>
2026-07-13 23:03:03 +0200

    Update stories-data.js

--- scripts/stories-data.js
+++ scripts/stories-data.js
@@ -7,9 +7,9 @@ const stories = [
 
 
 {
-id:"beginning",
+id:"treasur",
 
-title:"The Beginning",
+title:"The Legendary Treasur",
 
 
 cover:"📖",
@@ -17,22 +17,20 @@ cover:"📖",
 
 text:`
 
-Am Anfang entstand eine neue Welt.
+1540 an einem Hafen in einem kleinen Dorf namens Romna, liegt ein stark rot bemaltes Schiff. 
+Es hat schwarze Verzierungen und gehört dem Caipten Schifffahrer Dave. 
+Zur heutigen Zeit würde man ihn als Rentner und durchgeknallten zerbrechlichen Mann beschreiben, doch zu dieser Zeit ist er ein ehrenvoller, abenteuerlustiger und erfahrener Schifffahrer beschreiben.
+Seine Crew nennt ihn auch Fliezpiepenheimer. 
 
-Niemand wusste, welche Ereignisse bevorstanden.
-
-Alte Geheimnisse wurden entdeckt und eine neue Ära begann.
-
-
-Die ersten Zeichen erschienen am Horizont.
-
-Verborgene Kräfte erwachten.
-
-Menschen, die niemals hätten gefunden werden sollen,
-kehrten zurück.
+Heute ist es am Hafen etwas sonderbar, da die Crew das erstemal ein wichtige und gut bezahlte Fracht verschiffen sollen. 
+Die Crew schrubbt das Deck und bereitet schon alles auf diese wichtige Ladung vor. 
+Nach kurzem warten trifft die Ladung endlich ein, doch irgendwas ist komisch für den Captain. 
+Die Ladung ist in eine massive Metallbox mit 5 Schlössern verstaut. "Was da wohl drin ist?" denken sich alle. 
+Die zwei Männer die die Fracht hergebracht haben, warten die Crew deutlich die Box unter keinen Umständen aufzumachen, da dies der Tod aller auf dem Schiff bedeutet. 
+Dem Captain ist dies nicht ganz so geheuer jedoch braucht die Crew das Geld was es für die Auslieferung gibt dringend, weshalb er die Fracht aufladen lässt. 
+Die zwei Männer verschwieden schnell und hinterlassen nur einen leeren Hafen.
 
 
-Die Geschichte beginnt hier.
 
 
 `
