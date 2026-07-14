5555f3b0b1644d8a71a14f047923ac79446712b4
spitzefluke <spitzefluke@gmail.com>
2026-07-14 21:30:05 +0200

    Update stories-data.js

--- scripts/stories-data.js
+++ scripts/stories-data.js
@@ -18,16 +18,16 @@ cover:"📖",
 text:`
 
 1540 an einem Hafen in einem kleinen Dorf namens Romna, liegt ein stark rot bemaltes Schiff. 
-Es hat schwarze Verzierungen und gehört dem Caipten Schifffahrer Dave. 
-Zur heutigen Zeit würde man ihn als Rentner und durchgeknallten zerbrechlichen Mann beschreiben, doch zu dieser Zeit ist er ein ehrenvoller, abenteuerlustiger und erfahrener Schifffahrer beschreiben.
+Es hat schwarze Verzierungen und gehört dem Caiptain Schifffahrer Dave. 
+Zur heutigen Zeit würde man ihn als Rentner und durchgeknallten zerbrechlichen Mann beschreiben, doch zu dieser Zeit ist er ein ehrenvoller, abenteuerlustiger und erfahrener Schifffahrer.
 Seine Crew nennt ihn auch Fliezpiepenheimer. 
 
 Heute ist es am Hafen etwas sonderbar, da die Crew das erstemal ein wichtige und gut bezahlte Fracht verschiffen sollen. 
 Die Crew schrubbt das Deck und bereitet schon alles auf diese wichtige Ladung vor. 
 Nach kurzem warten trifft die Ladung endlich ein, doch irgendwas ist komisch für den Captain. 
 Die Ladung ist in eine massive Metallbox mit 5 Schlössern verstaut. "Was da wohl drin ist?" denken sich alle. 
-Die zwei Männer die die Fracht hergebracht haben, warten die Crew deutlich die Box unter keinen Umständen aufzumachen, da dies der Tod aller auf dem Schiff bedeutet. 
-Dem Captain ist dies nicht ganz so geheuer jedoch braucht die Crew das Geld was es für die Auslieferung gibt dringend, weshalb er die Fracht aufladen lässt. 
+Die zwei Männer die die Fracht hergebracht haben, warnten die Crew deutlich die Box unter keinen Umständen aufzumachen, da dies der Tod aller auf dem Schiff bedeutet. 
+Dem Captain ist dies nicht ganz so geheuer, jedoch braucht die Crew dringend das Geld was es für die Auslieferung gibt, weshalb er die Fracht aufladen lässt. 
 Die zwei Männer verschwieden schnell und hinterlassen nur einen leeren Hafen.
 
 
@@ -43,9 +43,9 @@ Die zwei Männer verschwieden schnell und hinterlassen nur einen leeren Hafen.
 
 
 {
-id:"lostworld",
+id:"treasure2",
 
-title:"Lost World",
+title:"The Legendary Treasur",
 
 
 cover:"📖",
@@ -53,15 +53,19 @@ cover:"📖",
 
 text:`
 
-Eine vergessene Welt wurde entdeckt.
+Das Schiff legt ab und die seltsame Fracht wurde in den Frachtraum geladen. Ihre Reise durch die weiten der Meere beginnt und führt die Crew durch verschiedene Herausforderungen.
+Einer der Crewmitglieder Finjan Backbeard, der jüngste und frechste von allen auf dem Schiff, ist ihr wichtigster Mann. Er ägert den Caiptain oft, jedoch ist er der beste Beobachter seines Gebietes. 
+Oben auf dem Auskuck hat er schon oft dafür gesorgt, dass die Crew nicht in ein Monster oder ein feindliches Schiff geraten sind. Auch heute ist er wieder auf seinem Sitz und hält ausschau. 
+Die Flagge die direkt gegenüber, etwas weiter unten, gehist ist, hat Finjan selbst entworfen. Darauf ist ein Mann mit Muskeln zusehen und im Hintergund ein Schiff, welches dem ähnelt mit dem sie auch unterwegs sind. 
 
-Jahrhunderte lang lag sie verborgen.
+Die Nacht bricht langsam ein und der erste Tag ihrer Reise neigt sich dem Ende, die meisten der Crew gehen in ihre Cojen und versuchen zu schlafen. Der Seegang ist noch verhältnismäßig zu vergangenen Abenteuern ruhig.
+Caiptain Dave gibt den Befehl für das Senken des Ankers und geht in sein Zimmer, wo er noch über Mögliche Gefahren nachdenkt. Doch seine Gedanken werden durch einen plötzlichen Wellengang gestört, er huscht durch die Tür aufs Deck und entdeckt in der Ferne zwei "Sauluppen". 
+Diese Tiere sind eine Mischung aus Walen und einem Schwein, sie sind um die 2 Meter groß und ungefährliche Tiere. Doch diese beiden "Sauluppen" sind in einen Kampf untereinander verwickelt, da es sich um zwei Weibchen handelt.
+Die Weibchen dieser Tiere sind sehr streitbereit und sind oft in Kämpfe mit anderen Weibchen verwickelt. Nur dieser Kampf ist anders findet der Caiptain. 
+Auch die anderen von der Crew sind wach geworden und beobachten den Kampf. Finjan drückt dem Caiptain seinen Ellenbogen in die Seite und gibt lachend ab "Ey Caiptain sind das nicht ihre Vorfahren?". 
+Der Caiptain lacht nur und tätschelt mit der Hand auf die Schultern von Finjan. "Pass bloß auf das ich dich nicht über Board schmeiße" erwiedert Dave. 
 
-Doch mit ihrer Entdeckung wurden auch alte Gefahren geweckt.
-
-
-Niemand wusste,
-was sich wirklich unter der Oberfläche befand.
+...
 
 
 `
