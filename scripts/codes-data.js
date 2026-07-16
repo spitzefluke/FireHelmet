d00d57d3ac21f3c36a5bae8ade76f97f199e46a5
spitzefluke <spitzefluke@gmail.com>
2026-07-16 19:59:59 +0200

    Add files via upload

---
+++ scripts/codes-data.js
@@ -0,0 +1,28 @@
+/* ======================================
+   CODE DATENBANK
+   Hier kannst du beliebig viele Codes und
+   die dazugehörige Nachricht eintragen.
+   Groß-/Kleinschreibung ist egal.
+====================================== */
+
+const codes = [
+  {
+    code: "Welcome",
+    message: "Willkommen an Bord, Crewmitglied! 🏴‍☠️ Du hast den ersten Geheimcode geknackt.",
+  },
+  {
+    code: "12035",
+    message: "Du bist ein kluger Mensch hier dein nächster Vorteil: 11490",
+  },
+  {
+    code: "20891",
+    message: "Ich bin zwar nicht groß doch bin immer zu sehen",
+  },
+  {
+    code: "KEY",
+    message: "PS: Schau auf Youtube",
+  },
+];
+
+// Nachricht, wenn der Code nicht gefunden wurde
+const codeNotFoundMessage = "❌ Dieser Code ist ungültig. Versuch es nochmal!";
