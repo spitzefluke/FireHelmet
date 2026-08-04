c1ce23d5852b3c194e45919f88562ecc2da91c33
spitzefluke <spitzefluke@gmail.com>
2026-08-04 18:38:29 +0200

    Add image path for Dean Silentmouth character

--- scripts/characters/characters-data.js
+++ scripts/characters/characters-data.js
@@ -21,7 +21,7 @@ const characters = [
   },
   {
     name: "Dean Silentmouth",
-    image: "",
+    image: "scripts/image/charater-1",
     description: "Der Steuermann des Schiffes und einer von Daves engsten Vertrauten. Redet nicht viel, aber kennt sich auf den Weltmeeren aus wie kaum ein anderer.",
   },
   {
