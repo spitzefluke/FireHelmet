053fa188beb9e588e14bbfa16c6011ef515d6e9d
spitzefluke <spitzefluke@gmail.com>
2026-08-08 21:26:13 +0200

    Add files via upload

---
+++ scripts/music/music-data.js
@@ -0,0 +1,18 @@
+/* ======================================
+   MUSIK PLAYLIST
+   Trage hier beliebig viele Songs ein.
+   Sie werden der Reihe nach abgespielt
+   und starten danach automatisch wieder
+   von vorne (Endlos-Playlist).
+   Pfad ist relativ zur index.html, deine
+   Songs liegen im Ordner "music/".
+====================================== */
+
+const musicPlaylist = [
+  "music/countdown-1.mp3",
+  "music/countdown-2.mp3",
+  "music/countdown-3.mp3",
+  "music/countdown-4.mp3",
+  "music/Weiter_hinter_dem_Fluss.mp3",
+  "music/Kaltes_Geigenhaus.mp3",
+];
