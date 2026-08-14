d9e1209aa041105ea7cada32453f959401e7cdb9
spitzefluke <spitzefluke@gmail.com>
2026-08-14 19:27:06 +0200

    Update streamraetsel-data.js

--- scripts/streamraetsel/streamraetsel-data.js
+++ scripts/streamraetsel/streamraetsel-data.js
@@ -6,7 +6,7 @@
 
 const streamRaetselConfig = {
   // Freischalt-Datum (Format: "JJJJ-MM-TTTHH:MM:SS")
-  unlockDate: "2026-08-14T20:30:00",
+  unlockDate: "2026-08-14T19:30:00",
 
   // Wird erst NACH dem Freischalten angezeigt (bzw. für dich
   // im Vorschau-Modus, siehe unten)
